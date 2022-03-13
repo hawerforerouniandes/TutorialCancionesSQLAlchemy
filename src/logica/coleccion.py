7a95dd4d8c42e94e743afc2f5bd2477308b6e96f
Hawer FORERO REY <hawerforero@gmail.com>
2022-03-12 20:24:46 -0500

    Add test

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -31,6 +31,15 @@ class Coleccion():
             return True
         else:
             return False
+            
+    def eliminar_album(self, album_id):
+       try:
+           album = session.query(Album).filter(Album.id == album_id).first()
+           session.delete(album)
+           session.commit()
+           return True
+       except:
+           return False
 
     def dar_album_por_id(self, album_id):
         return session.query(Album).get(album_id).__dict__
