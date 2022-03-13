47b9f5924f9b664b9b2f3c9b57bd5227180f2447
Hawer FORERO REY <hawerforero@gmail.com>
2022-03-12 20:39:11 -0500

    add comment

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -41,7 +41,7 @@ class Coleccion():
        except:
            return False
 
-    #Eliminar album
+    #dar_album_por_id
     def dar_album_por_id(self, album_id):
         return session.query(Album).get(album_id).__dict__
 
